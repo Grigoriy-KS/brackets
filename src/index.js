a4c3eb884a2334d2bde436c4247cd03f9e10fcd6
Grigoriy Ksyonz <KsyonzGS@gmail.com>
2024-10-21 22:02:46 +0300

    feat: create a function (mon, oct 21, 2024 10:02:10 pm)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  const pairs = Object.fromEntries(bracketsConfig);
+  const stack = [];
+  for (const char of str) {
+		if (pairs[stack.at(-1)] === char) {
+      stack.pop();
+    } else {
+			stack.push(char);
+    }
+  }
+	return stack.length === 0;
 }
